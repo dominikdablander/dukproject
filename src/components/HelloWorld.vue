6ae6987018b03fe60a97acdf95396de2a9af39ae
Dominik Dablander <dominik.dablander@icloud.com>
2023-11-23 17:44:08 +0100

    modified:   src/components/HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,12 @@
   <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
     <div>
       <div class="flex justify-center p-4 md:p-8" style="padding: 20px">
-        <div class=" flex overflow-x-auto">
-          <div v-for="(column, index) in columns" :key="index" class="w-96 md:w-auto">
+        <div class="flex overflow-x-auto">
+          <div
+            v-for="(column, index) in columns"
+            :key="index"
+            class="w-96 md:w-auto"
+          >
             <p
               class="text-gray-700 font-semibold font-sans tracking-wide text-sm"
             >
@@ -13,7 +17,7 @@
               :key="index"
               class="t-table"
               :class="{ 't-table-right': index === 0 }"
-              style="min-height: 300px;"
+              style="min-height: 300px"
               v-model="column.tasks"
               item-key="id"
               group="tasks"
@@ -32,7 +36,43 @@
         </div>
       </div>
     </div>
-    <div>div 2</div>
+    <div>
+      <div class="flex justify-center p-4 md:p-8">
+        <div class="flex overflow-x-auto">
+          <div
+            v-for="(column, index) in vorrat"
+            :key="index"
+            class="w-96 md:w-auto"
+          >
+            <p
+              class="text-left text-gray-700 font-semibold font-sans tracking-wide text-sm"
+            >
+              {{ column.title }}
+            </p>
+            
+            <draggable
+              :key="index"
+              class="table"
+              style="min-height: 300px"
+              v-model="column.tasks"
+              item-key="id"
+              group="tasks"
+              ghost-class="ghost-card"
+              :empty-insert-threshhold="50"
+            >
+              <template #item="{ element }">
+                <task-card
+                  class="mt-3 cursor-move"
+                  :task="element"
+                  :key="element.id"
+                ></task-card>
+              </template>
+            </draggable>
+          </div>
+        </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
@@ -45,6 +85,29 @@ export default {
   components: {
     draggable,
     TaskCard,
+  },
+
+  methods: {
+    checkTasks(tasks) {
+      // IDs der erwarteten Tasks
+      const expectedIDsSoll = [1, 2];
+      const expectedIDsHaben = [3, 4];
+
+      // IDs in den gegebenen Tasks überprüfen
+      const idsSoll = tasks.filter(task => task.id).map(task => task.id);
+      const idsHaben = tasks.filter(task => task.id).map(task => task.id);
+
+      // Überprüfung, ob die erwarteten IDs in den gegebenen Tasks enthalten sind
+      const isSollCorrect = expectedIDsSoll.every(id => idsSoll.includes(id));
+      const isHabenCorrect = expectedIDsHaben.every(id => idsHaben.includes(id));
+
+      // Ausgabe entsprechend der Überprüfung
+      if (isSollCorrect && isHabenCorrect) {
+        alert('Richtig!');
+      } else {
+        alert('Falsch!');
+      }
+    }
   },
 
   data() {
@@ -62,11 +125,10 @@ export default {
           date: "Sep 12",
         },
       ],
-      columns: [
-        {
-          title: "Soll",
-          tasks: [
-            {
+      vorrat: [
+        {title: "Vorrat",
+      tasks: [
+      {
               id: 1,
               title: "Anlagevermögen",
               date: "Sep 14",
@@ -77,12 +139,6 @@ export default {
               title: "Umlaufvermögen",
               date: "Sep 12",
             },
-            
-          ],
-        },
-        {
-          title: "Haben",
-          tasks: [
             {
               id: 3,
               title: "Eigenkapital",
@@ -95,9 +151,21 @@ export default {
               date: "Sep 14",
               type: "Feature Request",
             },
+      ]}
+      ],
+      columns: [
+        {
+          title: "Soll",
+          tasks: [],
+        },
+        {
+          title: "Haben",
+          tasks: [
             
           ],
         },
+
+      
       ],
     };
   },
@@ -106,6 +174,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+
+
 .t-table {
   border-top: 2px solid black;
   padding: 10px;
